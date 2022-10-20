3d58af6138f79f25b2ffe5655aba594934981a90
faizakram455 <115332826+faizakram455@users.noreply.github.com>
2022-10-20 15:03:07 +0530

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,49 @@
+const a = "faiz"
+switch(a)
+{
+    case  'Roshan' :
+    {
+    console.log("Mismatch");
+    }
+    break;
+    case  'Akshaya':
+    {
+        console.log("Mismatch");
+    }
+    break;
+    case   "Sakshi" :
+    {
+        console.log("Mismatch");
+    }
+    break;
+    case   "Arun":
+    {
+        console.log("Mismatch");
+    }
+    break;
+    case  "yaswanth" :
+    {
+        console.log("Mismatch");
+    }
+    break;
+
+default :
+console.log("No Name is Matched");
+}
+
+switch ("jan")
+{
+    case  'jan':
+        {
+      console.log("31 days");
+        } 
+     break;
+    case 'feb':
+        console.log("28 days");
+    break;
+    case 'mar' :
+        console.log("30 days");
+        break;
+      default:
+        console.log("No month is matched");
+}
